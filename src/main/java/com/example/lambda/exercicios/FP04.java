ea2732d6f9ebf9080ac1311e46e0b2802b704d3a
Bruna Franchi <bfzdev@gmai.com>
2023-05-15 19:13:35 -0300

    grouping, average

--- src/main/java/com/example/lambda/exercicios/FP04.java
+++ src/main/java/com/example/lambda/exercicios/FP04.java
@@ -66,13 +66,19 @@ public class FP04 {
                 new Course("Docker", "Cloud", 92, 20000)
         );
 
-        boolean b = courses.stream().allMatch(course -> course.getReviewScore() > 90);
+        boolean b = courses
+                .stream()
+                .allMatch(course -> course.getReviewScore() > 90);
         System.out.println("All courses match with score greater then 90? " + b);
 
-        boolean b1 = courses.stream().anyMatch(course -> course.getReviewScore() > 90);
+        boolean b1 = courses
+                .stream()
+                .anyMatch(course -> course.getReviewScore() > 90);
         System.out.println("Any course match with score greater then 90? " + b1);
 
-        boolean b2 = courses.stream().noneMatch(c -> c.getNoOfStudents() == 20005);
+        boolean b2 = courses
+                .stream()
+                .noneMatch(c -> c.getNoOfStudents() == 20005);
         System.out.println("None course match with 20005 students? " + b2);
 
         boolean b3 = courses.stream().anyMatch(c -> c.getNoOfStudents() < 5000);
@@ -80,38 +86,66 @@ public class FP04 {
 
         Comparator<Course> comparingByNoOfStudents = Comparator.comparing(Course::getNoOfStudents);
         Comparator<Course> comparingByNoOfStudentsReversed = Comparator.comparing(Course::getNoOfStudents).reversed();
-        List<Course> collect = courses.stream().sorted(comparingByNoOfStudents).collect(Collectors.toList());
-        List<Course> collectDecreasing = courses.stream().sorted(comparingByNoOfStudentsReversed).collect(Collectors.toList());
+        List<Course> collect = courses
+                .stream()
+                .sorted(comparingByNoOfStudents)
+                .collect(Collectors.toList());
+        List<Course> collectDecreasing = courses
+                .stream()
+                .sorted(comparingByNoOfStudentsReversed)
+                .collect(Collectors.toList());
         System.out.println(collect);
         System.out.println(collectDecreasing);
 
         Comparator<Course> comparingTwoItens = Comparator.comparing(Course::getNoOfStudents).thenComparing(Course::getReviewScore);
-        List<Course> collectTwoItens = courses.stream().sorted(comparingTwoItens).collect(Collectors.toList());
+        List<Course> collectTwoItens = courses
+                .stream()
+                .sorted(comparingTwoItens)
+                .collect(Collectors.toList());
         System.out.println(collectTwoItens);
 
         Comparator<Course> comparingNames = Comparator.comparing(Course::getName);
-        List<Course> collect1 = courses.stream().sorted(comparingNames).collect(Collectors.toList());
+        List<Course> collect1 = courses
+                .stream()
+                .sorted(comparingNames)
+                .collect(Collectors.toList());
         System.out.println(collect1);
 
         //limit
-        List<Course> collect2 = courses.stream().sorted(comparingTwoItens).limit(3).collect(Collectors.toList());
+        List<Course> collect2 = courses
+                .stream()
+                .sorted(comparingTwoItens)
+                .limit(3)
+                .collect(Collectors.toList());
         System.out.println(collect2);
 
-        List<Course> collect3 = courses.stream().sorted(comparingTwoItens).skip(2).limit(1).collect(Collectors.toList());
+        List<Course> collect3 = courses
+                .stream()
+                .sorted(comparingTwoItens)
+                .skip(2)
+                .limit(1)
+                .collect(Collectors.toList());
         System.out.println(collect3);
 
         List<Integer> numeros = List.of(1, 2, 3, 4, 5);
         //max - min -> retorna Optional
-        System.out.println("=== MAX ===");
-        Optional<Integer> max = numeros.stream().max(Comparator.comparingInt(Integer::intValue));
+        System.out.println("MAX");
+        Optional<Integer> max = numeros
+                .stream()
+                .max(Comparator.comparingInt(Integer::intValue));
         System.out.println(max);
 
-        System.out.println("=== MIN ===");
-        Optional<Course> min = courses.stream().min(comparingTwoItens);
+        System.out.println("MIN");
+        Optional<Course> min = courses
+                .stream()
+                .min(comparingTwoItens);
         System.out.println(min);
 
         System.out.println("EMPTY OPTIONAL");
-        System.out.println(courses.stream().filter(c -> c.getReviewScore() < 80).min(comparingTwoItens));
+        System.out.println(courses
+                .stream()
+                .filter(c -> c.getReviewScore() < 80)
+                .min(comparingTwoItens));
 
         System.out.println("EMPTY OPTIONAL -> OR ELSE");
         System.out.println(courses.stream().filter(c -> c.getReviewScore() < 80)
@@ -120,10 +154,34 @@ public class FP04 {
 
         //dropWhile
         System.out.println("DROPWHILE");
-        System.out.println(courses.stream().dropWhile(condition -> condition.getReviewScore() < 95).collect(Collectors.toList()));
+        System.out.println(courses
+                .stream()
+                .dropWhile(condition -> condition.getReviewScore() < 95)
+                .collect(Collectors.toList()));
 
         //takeWhile
         System.out.println("TAKE WHILE");
-        System.out.println(courses.stream().takeWhile(condition -> condition.getReviewScore() < 98).collect(Collectors.toList()));
+        System.out.println(courses
+                .stream()
+                .takeWhile(condition -> condition.getReviewScore() < 98)
+                .collect(Collectors.toList()));
+
+        //average
+        System.out.println("AVERAGE");
+        System.out.println(courses.stream()
+                .filter(c -> c.getReviewScore() > 95)
+                .mapToInt(Course::getNoOfStudents)
+                        .average().stream().max()
+                );
+
+        //grouping
+        courses
+                .stream()
+                .collect(Collectors.groupingBy(Course::getCategory));
+
+        courses
+                .stream()
+                .collect(Collectors.groupingBy(Course::getCategory, Collectors.counting()));
+
     }
 }
